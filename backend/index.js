809246cd93750479743bb96336804ede8252a7f7
saikumar2063 <saikumar.2063@gmail.com>
2024-05-30 13:32:57 +0530

    six commit

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+let cors = require("cors");
+
 const app = express();
 const port = 4000;
 const mongoDB = require("./db");
 mongoDB();
+
+app.use(cors());
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
