e4aa6471f0d6336269fa2c97c4ff46a6cec12fe9
saikumar2063 <saikumar.2063@gmail.com>
2024-05-31 15:44:10 +0530

    tenth commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ function Footer() {
             to="/"
             className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"
           ></Link>
-          <span className="text-muted">© 2021 FoodMunch, Inc</span>
+          <span className="text-muted">© 2024 FoodMunch, Inc</span>
         </div>
       </footer>
     </div>
